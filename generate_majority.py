e569fe28358d1d2df84a04a2950b16df1aa8dc08
Matej Klemen <matej.klemen1337@gmail.com>
2026-03-12 13:45:20 +0100

    Clean up generate_majority.py

--- generate_majority.py
+++ generate_majority.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 from collections import Counter
 
@@ -5,65 +6,60 @@ import pandas as pd
 
 from constants import UD_TEST_PATHS as UD_PATHS
 
-## Majority answers are generated according to WALS (can't rely on training distribution, not all treebanks have a training set)
+parser = argparse.ArgumentParser()
+parser.add_argument("--feature_name", type=str, default="81A", help="Specific feature ID or 'all'")
 
-feature_name = "81A"
-MAJORITY_ANSWER = "SOV"
-
-# feature_name = "82A"
-# MAJORITY_ANSWER = "SV"
-
-# feature_name = "83A"
-# MAJORITY_ANSWER = "OV"
-
-# feature_name = "84A"
-# MAJORITY_ANSWER = "VOX"
-
-# feature_name = "85A"
-# MAJORITY_ANSWER = "Postpositions"
-
-# feature_name = "86A"
-# MAJORITY_ANSWER = "GenN"
-
-# feature_name = "87A"
-# MAJORITY_ANSWER = "NAdj"
-
-# feature_name = "88A"
-# MAJORITY_ANSWER = "DemN"
-
-# feature_name = "89A"
-# MAJORITY_ANSWER = "NNum"
-
-# feature_name = "90A"
-# MAJORITY_ANSWER = "NRel"
-
-# feature_name = "94A"
-# MAJORITY_ANSWER = "Adverbial subordinators which appear at the beginning of the subordinate clause"  # initial
-
-# feature_name = "144A"
-# MAJORITY_ANSWER = "Morphological negation"
-
-# feature_name = "144B"
-# MAJORITY_ANSWER = "Immediately preverbal"
-
-problem_response_data = {
-    "UD_Language": [], "WALS_Language": [],
-    "Correct_value": [], "LLM_Response": [],
-    "LLM_Response_postp": [], "LLM_Response_unagg": []
+# Majority answers are generated according to WALS (can't rely on training distribution, not all treebanks have a training set)
+MAJORITY_ANSWERS_WALS = {
+    "81A": "SOV",
+    "81B": "SOV or SVO",
+    "82A": "SV",
+    "83A": "OV",
+    "84A": "VOX",
+    "85A": "Postpositions",
+    "86A": "GenN",
+    "87A": "NAdj",
+    "88A": "DemN",
+    "89A": "NNum",
+    "90A": "NRel",
+    "94A": "Adverbial subordinators which appear at the beginning of the subordinate clause",
+    "95A": "OV&Postpositions",
+    "96A": "VO&NRel",
+    "97A": "VO&NAdj",
+    "144A": "Morphological negation",
+    "144B": "Immediately preverbal"
 }
 
-for curr_lang_ud, ud_path in UD_PATHS.items():
-    problem_response_data["UD_Language"].append(curr_lang_ud)
-    problem_response_data["WALS_Language"].append("/")
-    problem_response_data["Correct_value"].append("/")
-    problem_response_data["LLM_Response"].append(MAJORITY_ANSWER)
-    problem_response_data["LLM_Response_postp"].append("")
-    problem_response_data["LLM_Response_unagg"].append(str(Counter({MAJORITY_ANSWER: 1})))
+# Creates majority answers for a feature based on the WALS dominant value
+if __name__ == "__main__":
+    args = parser.parse_args()
+    experiment_dir = "majority_baseline"
+    os.makedirs(experiment_dir, exist_ok=True)
 
-problem_response_data = pd.DataFrame(problem_response_data)
+    features_to_run = []
+    if args.feature_name == "all":
+        features_to_run.extend(list(MAJORITY_ANSWERS_WALS.keys()))
+    else:
+        features_to_run.append(args.feature_name)
 
-experiment_dir = "majority_baseline"
-os.makedirs(experiment_dir, exist_ok=True)
-os.makedirs(os.path.join(experiment_dir, feature_name), exist_ok=True)
-problem_response_data.to_csv(os.path.join(experiment_dir, feature_name, f"answers_{feature_name}.tsv"),
-                             sep="\t", index=False)
+    for _feat in features_to_run:
+        majority_answer = MAJORITY_ANSWERS_WALS[_feat]
+        problem_response_data = {
+            "UD_Language": [], "WALS_Language": [],
+            "Correct_value": [], "LLM_Response": [],
+            "LLM_Response_postp": [], "LLM_Response_unagg": []
+        }
+
+        for curr_lang_ud, ud_path in UD_PATHS.items():
+            problem_response_data["UD_Language"].append(curr_lang_ud)
+            problem_response_data["WALS_Language"].append("/")
+            problem_response_data["Correct_value"].append("/")
+            problem_response_data["LLM_Response"].append(majority_answer)
+            problem_response_data["LLM_Response_postp"].append("")
+            problem_response_data["LLM_Response_unagg"].append(str(Counter({majority_answer: 1})))
+
+        problem_response_data = pd.DataFrame(problem_response_data)
+
+        os.makedirs(os.path.join(experiment_dir, _feat), exist_ok=True)
+        problem_response_data.to_csv(os.path.join(experiment_dir, _feat, f"answers_{_feat}.tsv"),
+                                     sep="\t", index=False)
